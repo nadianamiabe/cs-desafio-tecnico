0eb2bec91d3b906bc856f58747940f227cc194bc
nadianamiabe <abe.nami0510@gmail.com>
2023-08-25 16:42:14 -0300

    clearsale-2nd-challenge update main and add tests

--- challenge-2/src/main_api.ts
+++ challenge-2/src/main_api.ts
@@ -6,6 +6,9 @@ import HttpServerHapiAdapter from './http/HttpServerHapiAdapter';
 import RickAndMortyAPIClientGatewayImpl from './client/RickAndMortyAPIClientGatewayImpl';
 import HttpClientAxiosAdapter from './http/HttpClientAxiosAdapter';
 import GetOneCharacterUseCase from './usecase/GetOneCharacterUseCase';
+import InsertCharacterUseCase from './usecase/InsertCharacterUseCase';
+import UpdateCharacterUseCase from './usecase/UpdateCharacterUseCase';
+import DeleteCharacterUseCase from './usecase/DeleteCharacterUseCase';
 
 // main
 const repository = new InMemoryCharacterRepository();
@@ -19,8 +22,20 @@ const getOneCharacterUseCase = new GetOneCharacterUseCase(
   repository,
   rickAndMortyApiGateway,
 );
-// const httpServer = new HttpServerExpressAdapter();
-const httpServer = new HttpServerHapiAdapter();
 
-new HttpController(httpServer, getAllCharactersUseCase, getOneCharacterUseCase);
+const insertCharacterUseCase = new InsertCharacterUseCase(repository);
+const updateCharacterUseCase = new UpdateCharacterUseCase(repository);
+const deleteCharacterUseCase = new DeleteCharacterUseCase(repository);
+
+const httpServer = new HttpServerExpressAdapter();
+// const httpServer = new HttpServerHapiAdapter();
+
+new HttpController(
+  httpServer,
+  getAllCharactersUseCase,
+  getOneCharacterUseCase,
+  insertCharacterUseCase,
+  updateCharacterUseCase,
+  deleteCharacterUseCase,
+);
 httpServer.listen(3000);
--- challenge-2/test/main_api.test.ts
+++ challenge-2/test/main_api.test.ts
@@ -9,3 +9,32 @@ test('Should return one character', async () => {
   const response = await axios.get('http://localhost:3000/character/1');
   expect(response.status).toBe(200);
 });
+
+test('Should insert one character', async () => {
+  const input = {
+    name: 'Teste teste',
+    status: 'Alive',
+    species: 'Feline',
+    gender: 'Male',
+    image: 'https://cdn.pixabay.com/photo/2014/11/30/14/11/cat-551554_640.jpg',
+    url: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fimages%2Fsearch%2Fcat%2F&psig=AOvVaw3XqBuR0TTbdYlaE0oHfRQM&ust=1692972480352000&source=images&cd=vfe&opi=89978449&ved=0CA4QjRxqFwoTCODt9Yu89YADFQAAAAAdAAAAABAE',
+  };
+  const response = await axios.post('http://localhost:3000/character', input);
+  expect(response.status).toBe(201);
+});
+
+test('Should update one character', async () => {
+  const input = {
+    status: 'Alive',
+    species: 'Feline',
+    gender: 'Male',
+    image: 'https://cdn.pixabay.com/photo/2014/11/30/14/11/cat-551554_640.jpg',
+  };
+  const response = await axios.put('http://localhost:3000/character/1', input);
+  expect(response.status).toBe(200);
+});
+
+test('Should delete one character', async () => {
+  const response = await axios.delete('http://localhost:3000/character/1');
+  expect(response.status).toBe(204);
+});
